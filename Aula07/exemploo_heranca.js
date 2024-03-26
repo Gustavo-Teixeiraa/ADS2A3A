142921f7eb5e0f6d6cce5ab6371cf4e1b2b4dcf1
Gustavo <Gusta@gmail.com>
2024-03-26 10:00:26 -0300

    Aula 07 completa

--- Aula07/exemploo_heranca.js
+++ Aula07/exemploo_heranca.js
@@ -2,8 +2,12 @@ class Conta{
     constructor(){
         this.Saldo = 0;
     }
-    get Saldo(){return this.Saldo}
-    set Saldo(pSaldo){this.Saldo = pSaldo}
+    get Saldo(){return this.saldo}
+    set Saldo(pSaldo){this.saldo = pSaldo}
+
+    imprimir(){
+        return "Saldo: " + this.saldo;
+    }
 }
 
 class Corrente extends Conta {
@@ -12,8 +16,18 @@ class Corrente extends Conta {
         this.Limite = pLimite;
     }
     get Limite(){return this.limite}
-    set Limite(pLimite){this.Saldo = pLimite}
+    set Limite(pLimite){this.limite = pLimite}
+    
+    imprimir(){
+        return super.imprimir() + "Limite: " + this.Limite
+    }
 }
 
-var obj_cc = new Corrente(1000);
-console.log(obj_conta)
+/*var obj_conta = new Conta();
+obj_conta.saldo = 700
+console.log(obj_conta.imprimir());
+*/
+
+var obj_cc = new Corrente(1000)
+obj_cc.saldo = 700;
+console.log(obj_cc.imprimir());
